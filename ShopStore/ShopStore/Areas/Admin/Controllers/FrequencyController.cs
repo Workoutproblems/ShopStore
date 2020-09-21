a4b6ce370c7065d49b4369f3d23d5d05ac3b97eb
johnny <johnnysoto961@hotmail.com>
2020-09-20 22:01:08 -0500

    Frequency CRUD complete.

--- ShopStore/ShopStore/Areas/Admin/Controllers/FrequencyController.cs
+++ ShopStore/ShopStore/Areas/Admin/Controllers/FrequencyController.cs
@@ -25,47 +25,47 @@ namespace ShopStore.Areas.Admin.Controllers
 
 
         // GET: Contacts/Upsert/5
-        //public IActionResult Upsert(int? id)
-        //{
-        //    Frequency frequency = new Frequency();
-        //    if (id == null)
-        //    {
+        public IActionResult Upsert(int? id)
+        {
+            Frequency frequency = new Frequency();
+            if (id == null)
+            {
 
-        //        return View(frequency);
-        //    }
+                return View(frequency);
+            }
 
-        //    frequency = _unitOfWork.Frequency.Get(id.GetValueOrDefault());
-        //    if (frequency == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(frequency);
-        //}
+            frequency = _unitOfWork.Frequency.Get(id.GetValueOrDefault());
+            if (frequency == null)
+            {
+                return NotFound();
+            }
+            return View(frequency);
+        }
 
         // POST: Contacts/Upsert/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Upsert(Frequency frequency)
-        //{
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Frequency frequency)
+        {
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        if (frequency.Id == 0)
-        //        {
-        //            _unitOfWork.Frequency.Add(frequency);
-        //        }
-        //        else
-        //        {
-        //            _unitOfWork.Frequency.Update(frequency);
-        //        }
-        //        _unitOfWork.Save();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(frequency);
+            if (ModelState.IsValid)
+            {
+                if (frequency.Id == 0)
+                {
+                    _unitOfWork.Frequency.Add(frequency);
+                }
+                else
+                {
+                    _unitOfWork.Frequency.Update(frequency);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(frequency);
 
-        //}
+        }
 
 
         #region API Calls
@@ -76,19 +76,19 @@ namespace ShopStore.Areas.Admin.Controllers
             return Json(new { data = _unitOfWork.Frequency.GetAll() });
         }
 
-        //[HttpDelete]
-        //public IActionResult Delete(int id)
-        //{
-        //    var objFromDb = _unitOfWork.Frequency.Get(id);
-        //    if (objFromDb == null)
-        //    {
-        //        return Json(new { success = false, message = "Error while deleting." });
-        //    }
-        //    _unitOfWork.Frequency.Remove(objFromDb);
-        //    _unitOfWork.Save();
-        //    return Json(new { success = true, message = "Delete success." });
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Frequency.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _unitOfWork.Frequency.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete success." });
 
-        //}
+        }
         #endregion
     }
 }
